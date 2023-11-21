5e9e233f58ce86e4277589572b1fb7f960ad4c0f
aziza <azizafazliddinova08@gmail.com>
2023-11-21 14:12:31 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+// 1 задание
+// function checkAge(age) {
+//     if (age >= 18) {
+//       alert( 'Доступ получен' )
+//     }else  {
+//        alert( 'Доступ закрыт' )
+//     }
+//   }
+
+//   let age = prompt('Сколько вам лет?');
+
+//   checkAge(age) 
+
+
+
+// 2 задание
+// let a = prompt('число а')
+// let b = prompt('число b')
+// let oper = prompt('/  *  -  + ')
+// let z 
+
+// function calculate(num) {
+//     if (oper == '/') {
+//         z = num(a) / num(b)
+//         alert()
+//     } else if (oper == '*') {
+//         z = num(a) * num(b)
+//         alert('otwet' + z)
+//     } else if (oper == '-') {
+//         z = num(a) - num(b)
+//         alert('otwet' + z)
+//     } else if (oper == '+') {
+//         z = num(a) + num(b)
+//         alert('otwet' + z)
+//     }
+// }
+
+//далер ака я не понимаю что тут не правильно, я старалась
+
+
+
+//3 задание
+// function max_name(name1, name2, name3) {
+//     let max = name1
+//     if (name2.length > max.length) {
+//       max=name2
+//     }
+//     if (name3.length > max.length) {
+//       max=name3
+//     }
+//     return max
+//   }
+//   console.log(max_name('Alex', 'George', 'Michael'));
+
+//4 задание 
+//...
+
+// 5 задание
+// function proportcii(a, b, c) {
+//     return (a / b) * c;
+//   } 
+//   console.log( proportcii(20, 100, 50))
+  
