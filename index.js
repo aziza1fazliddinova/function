00c95e099f6c2e3524eaefebdbc56a042ae8376a
aziza <azizafazliddinova08@gmail.com>
2023-11-23 13:40:40 +0500

    aaa

--- index.js
+++ index.js
@@ -35,8 +35,7 @@
 //     }
 // }
 
-//далер ака я не понимаю что тут не правильно, я старалась
-
+// далер ака я не понимаю что тут не правильно, я старалась
 
 
 //3 задание
@@ -50,14 +49,65 @@
 //     }
 //     return max
 //   }
-//   console.log(max_name('Alex', 'George', 'Michael'));
+//   console.log(max_name('Alex', 'George', 'Michael'))
+
 
 //4 задание 
 //...
 
 // 5 задание
 // function proportcii(a, b, c) {
-//     return (a / b) * c;
+//     return (a / b) * c
 //   } 
 //   console.log( proportcii(20, 100, 50))
-  
+
+
+
+// //21.11.2023
+// function fibonacci(k) {
+//     let a = [1, 2]
+//     if (k <= 2) {
+//         return a.slice(1, k)
+//     }
+//     for (let i = 2; i < k; i++) {
+//         let next = a[i - 1] + a[i - 2]
+//         a.push(next)
+//     }
+//     return a
+// }
+// let num = prompt('введиту число')
+// let b = fibonacci(num)
+// console.log(b);
+
+function sort(arr) {
+
+    let sorted = false
+
+    while (!sorted) {
+      sorted = true
+      for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+          [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+          sorted = false
+        }
+      }
+    }
+    return arr
+  }
+
+  let numbers = [5, 2, 8, 1, 9 , 33]
+  console.log(sort(numbers))
+
+
+
+
+function names(a,b) {
+  let email =`${a.toLowerCase()}${b.toLowerCase()}@emaile.com;`
+  return email
+
+}
+  let name1 = prompt('Введите имя')
+  let name2 = prompt('Введите фамилию')
+  let email = names(name1, name2)
+  console.log(email)
+  names()
